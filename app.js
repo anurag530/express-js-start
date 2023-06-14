3048989a9f41a68b98ed334680562064940d0f0e
anurag530 <anurag.shukla530@gmail.com>
2023-06-14 18:05:44 +0530

    url home about node

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
-console.log("anurag shukla....");
-res.end("Anurag shukla");
+const url=req.url;
+if(url==='/node'){
+    //res.setHeader('content-type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My first Page</title></head>');
+    res.write('<body><h1> Welcome to my Node Js project</h1></body>');
+    res.write('</html>');
+    return res.end();
+}
+else if(url==='/about'){
+   // res.setHeader('content-type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My first Page</title></head>');
+    res.write('<body><h1>Welcome to About Us page</h1></body>');
+    res.write('</html>');
+    return res.end();
+}
+else(url==='/home')
+   // res.setHeader('content-type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My first Page</title></head>');
+    res.write('<body><h1>Welcome home</h1></body>');
+    res.write('</html>');
+   return res.end();
+
+
 });
 server.listen(4000,()=>console.log("sever started"));
