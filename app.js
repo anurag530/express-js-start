af6ea5a89aa5204a6bdfd424816de64f4b82373e
anurag530 <anurag.shukla530@gmail.com>
2023-06-17 19:45:06 +0530

    module file arrange concept

--- app.js
+++ app.js
@@ -1,41 +1,8 @@
 //writing and reading from a file
 
 const http=require('http');
-const fs=require('fs');
-const server=http.createServer((req,res)=>{
-const url=req.url;
-const method=req.method;
-if(url==='/'){
-    fs.readFile('message.txt',{encoding:"utf-8"},(err,data)=>{
-      if(err){
-        console.log(err);
-      } 
-      console.log(`data from file` + data);
-      res.write('<html>');
-    res.write('<head><title>Enter message</title></head>');
-    res.write(`<body> ${data} </body>`);
-    res.write(`<body><form action="/message" method="post"><input type="text" name="message"><button type="submit">send</button></form></body>`);
-    res.write('</html>');
-    return res.end();
-    });
-}
- if(url==='/message' && method==='POST'){
-    const body=[];
-    req.on('data',(chunk)=>{
-        console.log(chunk);
-        body.push(chunk);
-    });
-    req.on('end',()=>{
-        const parseBody=Buffer.concat(body).toString();
-        const message=parseBody.split('=')[1];
-        fs.writeFileSync('message.txt',message);
-        res.statusCode=302;
-       res.setHeader('Location','/');
-       return res.end();
-       
-    });
-}
+const route=require('./routes');
 
-});
+const server=http.createServer(route);
 
 server.listen(4000,()=>console.log("sever started"));
