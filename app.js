9ab2b2ddeefb79193ab7c7d5ccd2471ee36e63f1
anurag530 <anurag.shukla530@gmail.com>
2023-06-17 18:28:00 +0530

    reading and writing from file

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
+//writing and reading from a file
+
 const http=require('http');
+const fs=require('fs');
 const server=http.createServer((req,res)=>{
 const url=req.url;
-if(url==='/node'){
-    //res.setHeader('content-type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My first Page</title></head>');
-    res.write('<body><h1> Welcome to my Node Js project</h1></body>');
+const method=req.method;
+if(url==='/'){
+    fs.readFile('message.txt',{encoding:"utf-8"},(err,data)=>{
+      if(err){
+        console.log(err);
+      } 
+      console.log(`data from file` + data);
+      res.write('<html>');
+    res.write('<head><title>Enter message</title></head>');
+    res.write(`<body> ${data} </body>`);
+    res.write(`<body><form action="/message" method="post"><input type="text" name="message"><button type="submit">send</button></form></body>`);
     res.write('</html>');
     return res.end();
+    });
 }
-else if(url==='/about'){
-   // res.setHeader('content-type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My first Page</title></head>');
-    res.write('<body><h1>Welcome to About Us page</h1></body>');
-    res.write('</html>');
-    return res.end();
+ if(url==='/message' && method==='POST'){
+    const body=[];
+    req.on('data',(chunk)=>{
+        console.log(chunk);
+        body.push(chunk);
+    });
+    req.on('end',()=>{
+        const parseBody=Buffer.concat(body).toString();
+        const message=parseBody.split('=')[1];
+        fs.writeFileSync('message.txt',message);
+        res.statusCode=302;
+       res.setHeader('Location','/');
+       return res.end();
+       
+    });
 }
-else(url==='/home')
-   // res.setHeader('content-type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My first Page</title></head>');
-    res.write('<body><h1>Welcome home</h1></body>');
-    res.write('</html>');
-   return res.end();
-
 
 });
+
 server.listen(4000,()=>console.log("sever started"));
