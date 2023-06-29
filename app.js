2cfa60261606ec80a1f1f7497a6be4a72e400fb9
anurag530 <anurag.shukla530@gmail.com>
2023-06-29 17:02:26 +0530

    express starting

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-//writing and reading from a file
+// express starting----
 
-const http=require('http');
-const route=require('./routes');
+//const http=require('http');    //it is not required in express
 
-const server=http.createServer(route);
+const express= require('express');
 
-server.listen(4000,()=>console.log("sever started"));
+const app=express();
+
+app.use((req,res,next)=>{
+  console.log('In the middleware!');
+  next();  // it allows the request to continue to the next middleware in line
+});
+app.use((req,res,next)=>{
+    console.log('In the another middleware!');
+   // res.send('<h1>Hello From express!</h1>');
+   res.send({ key1: 5 });
+  });
+
+//const server=http.createServer(app); //it is not require in express
+
+app.listen(4000,()=>console.log("sever started"));
